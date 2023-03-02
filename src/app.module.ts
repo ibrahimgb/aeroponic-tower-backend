2126e72bc1fb8e3709c296694ee35086843dfe75
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-03-02 18:07:06 +0100

    add nest configs and update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaDbService } from './prisma_db/prisma_db.service';
+import { PrismaDbModule } from './prisma_db/prisma_db.module';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaDbModule,
+    UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaDbService],
 })
 export class AppModule {}
