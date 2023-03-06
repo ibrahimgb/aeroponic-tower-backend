e65cd65c675b67d4a103242993d48e19ea22ef83
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-03-06 18:33:54 +0100

    CRUD for aeroponic tower

--- src/aeroponic-tower/aeroponic-tower.controller.ts
+++ src/aeroponic-tower/aeroponic-tower.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Post } from '@nestjs/common';
+import { AeroponicTowerService } from './aeroponic-tower.service';
+import { AddAeroponicTowerDot, changePumpIntervalDto } from './dot';
+import { SensorData, PumpInterval } from '@prisma/client';
 
 @Controller('aeroponic-tower')
-export class AeroponicTowerController {}
+export class AeroponicTowerController {
+  constructor(private aeroponicTowerService: AeroponicTowerService) {
+    //
+  }
+
+  @Post('addtower')
+  addTower(@Body() tower: AddAeroponicTowerDot) {
+    return this.aeroponicTowerService.addTower(tower);
+  }
+
+  @Get('pumpInterval')
+  pumpInterval() {
+    return this.aeroponicTowerService.getAllPumpInterval();
+  }
+
+  @Post('newPumpInterval')
+  newPumpInterval(@Body() pumpInterval: PumpInterval) {
+    return this.aeroponicTowerService.newPumpInterval(pumpInterval);
+  }
+
+  @Patch('changePumpInterval')
+  changePumpInterval(@Body() data: changePumpIntervalDto) {
+    return this.aeroponicTowerService.changePumpInterval(data);
+  }
+}
--- src/aeroponic-tower/aeroponic-tower.service.ts
+++ src/aeroponic-tower/aeroponic-tower.service.ts
@@ -1,4 +1,65 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaDbService } from 'src/prisma_db/prisma_db.service';
+import { AddAeroponicTowerDot, changePumpIntervalDto } from './dot';
 
 @Injectable()
-export class AeroponicTowerService {}
+export class AeroponicTowerService {
+  constructor(private prisma: PrismaDbService) {}
+
+  addTower(tower: any) {
+    const newTower = this.prisma.aeroponicTower.create({
+      data: {
+        ...tower,
+      },
+    });
+
+    return newTower;
+  }
+
+  async getAllPumpInterval() {
+    const pumpInterval = await this.prisma.pumpInterval.findMany();
+  }
+
+  async newPumpInterval(pumpInterval: any) {
+    const newPumpInterval = await this.prisma.pumpInterval.create({
+      data: {
+        ...pumpInterval,
+      },
+    });
+
+    return newPumpInterval;
+  }
+
+  async changePumpInterval(data: changePumpIntervalDto) {
+    const pumpIntervalDosExist = this.prisma.pumpInterval.findFirst({
+      where: {
+        timeOff: data.timeOff,
+        timeOn: data.timeOn,
+      },
+    });
+
+    if (pumpIntervalDosExist) {
+      const towerSettingsChanged = this.prisma.aeroponicTower.update({
+        where: {
+          id: data.AeroponicTowerID,
+        },
+        data: {
+          pumpIntervalID: (await pumpIntervalDosExist).id,
+        },
+      });
+      return towerSettingsChanged;
+    }
+    const newPumpInterval = this.newPumpInterval({
+      timeOff: data.timeOff,
+      timeOn: data.timeOn,
+    });
+    const towerSettingsChanged = this.prisma.aeroponicTower.update({
+      where: {
+        id: data.AeroponicTowerID,
+      },
+      data: {
+        pumpIntervalID: (await newPumpInterval).id,
+      },
+    });
+  }
+}
