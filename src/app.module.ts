94d03be453bc5bab4a8f4746ba52a7c60076b47d
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-03-06 18:34:22 +0100

    app.module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,18 @@ import { PrismaDbService } from './prisma_db/prisma_db.service';
 import { PrismaDbModule } from './prisma_db/prisma_db.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { SensorService } from './sensor/sensor.service';
+import { SensorController } from './sensor/sensor.controller';
+import { AeroponicTowerService } from './aeroponic-tower/aeroponic-tower.service';
+import { AeroponicTowerController } from './aeroponic-tower/aeroponic-tower.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaDbModule,
-    UserModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaDbService],
+    UserModule,
+  ],
+  controllers: [AppController, SensorController, AeroponicTowerController],
+  providers: [AppService, PrismaDbService, SensorService, AeroponicTowerService],
 })
 export class AppModule {}
