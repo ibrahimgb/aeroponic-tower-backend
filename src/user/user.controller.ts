8008ceedf2203d4580aa9e6cbe44624fdc641577
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-04-01 18:36:08 +0100

    add guards and upload profile picture

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,35 @@
-import { Body, Controller, Get, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  Post,
+  Request,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { User } from '@prisma/client';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
 import { UserService } from './user.service';
+import path = require('path');
 
+export const storage = {
+  storage: diskStorage({
+    destination: './uploads/profileimages',
+    filename: (req, file, cb) => {
+      const filename: string =
+        path.parse(file.originalname).name.replace(/\s/g, '') + uuidv4();
+      const extension: string = path.parse(file.originalname).ext;
+
+      cb(null, `${filename}${extension}`);
+    },
+  }),
+};
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {
@@ -20,5 +48,24 @@ export class UserController {
   @Patch()
   editUser(@Body() user: any) {
     return this.userService.editUser(user);
+  }
+
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+
+  @UseGuards(JwtGuard)
+  @Post('uploadAvatar')
+  @UseInterceptors(FileInterceptor('file', storage))
+  uploadFile(@UploadedFile() file, @Request() req) {
+    const user: User = req.user;
+
+    return this.userService.addAvatar(file.filename, user.id);
+    // .pipe(
+    //     tap((user: User) => console.log(user)),
+    //     map((user:User) => ({profileImage: user.profileImage}))
+    // )
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -34,6 +34,8 @@ export class UserService {
         avatar: filename,
       },
     });
+
+    return editedUser;
   }
 
   async removeAvatar(id: number) {
