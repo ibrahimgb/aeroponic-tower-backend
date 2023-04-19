9bf8341bc513070ee1e07c42bc3668658c508bdf
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-04-19 02:28:43 +0100

    get port from .env file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  const config = new ConfigService();
+  const PORT = config.get('PORT') || 3000;
+  console.log('live on port ' + PORT);
+  await app.listen(PORT);
 }
 bootstrap();
